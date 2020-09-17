d38d6b15f7a75ff44a6f07983735bbf5a631191b
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-17 21:48:52 +0200

    change class name

--- app/src/main/java/ch/heigvd/iict/sym/labo1/SuccessActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/SuccessActivity.kt
@@ -3,9 +3,9 @@ package ch.heigvd.iict.sym.labo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Sucess : AppCompatActivity() {
+class SuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sucess)
+        setContentView(R.layout.activity_success)
     }
 }
