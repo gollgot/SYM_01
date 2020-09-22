7db7ef02f096369feecd4425c920de18e81774de
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-22 11:42:39 +0200

    Fetch a image when connection success

--- app/src/main/java/ch/heigvd/iict/sym/labo1/SuccessActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/SuccessActivity.kt
@@ -2,9 +2,14 @@ package ch.heigvd.iict.sym.labo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class SuccessActivity : AppCompatActivity() {
+
+    private lateinit var successActivityConnectedImageView: ImageView
+    private lateinit var successActivityUserEmailTextView : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +17,14 @@ class SuccessActivity : AppCompatActivity() {
         setContentView(R.layout.activity_success)
 
         // Récupère le mail de l'activité appelant et l'affiche sur l'écran
-        val textView = findViewById<TextView>(R.id.success_user_email).apply{
+        successActivityUserEmailTextView = findViewById<TextView>(R.id.success_user_email).apply {
             text = intent.getStringExtra(EXTRA_EMAIL)
         }
 
-        
+        // Récupère l'image depuis le lien et l'afficher sur l'activité
+        successActivityConnectedImageView = findViewById<ImageView>(R.id.success_connected_image)
+        ImageDownloader(successActivityConnectedImageView, "https://thispersondoesnotexist.com/image").show()
+
 
     }
 }
