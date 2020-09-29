aba8a302af693d456a5d6743afeffe7b66ec485d
Robin <r.demarta@outlook.com>
2020-09-29 21:11:08 +0200

    Display image from internet

--- app/src/main/java/ch/heigvd/iict/sym/labo1/HomeActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/HomeActivity.kt
@@ -2,18 +2,25 @@ package ch.heigvd.iict.sym.labo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
-import org.w3c.dom.Text
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var emailText: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var connectedImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        emailText = findViewById(R.id.home_email)
-        emailText.text = intent.getStringExtra("email")
+        // Display user email
+        emailTextView = findViewById(R.id.home_email)
+        emailTextView.text = intent.getStringExtra("email")
+
+        // Fetch image from internet
+        connectedImageView = findViewById(R.id.home_image)
+        ImageDownloader(connectedImageView, "https://thispersondoesnotexist.com/image").show()
     }
 }
