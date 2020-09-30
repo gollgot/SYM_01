50cdb4e34d572b0c176d593da4828c3d7078607d
Loïc Dessaules <loic.dessaules@gmail.com>
2020-09-30 21:32:57 +0200

    Display image profile from internet

--- app/src/main/java/ch/heigvd/iict/sym/labo1/ProfileActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/ProfileActivity.kt
@@ -3,7 +3,9 @@ package ch.heigvd.iict.sym.labo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -12,7 +14,10 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         val email: TextView = findViewById(R.id.profileTvEmail)
+        val imgProfile: ImageView = findViewById(R.id.profileImgProfile)
+
         email.text = intent.getStringExtra("email")
+        ImageDownloader(imgProfile, "https://thispersondoesnotexist.com/image").show()
     }
 
 }
